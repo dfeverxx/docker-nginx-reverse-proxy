a6843c15f40a0207fbae19f4278c4939dbba01d6
dfeverx <dfeverx@gmail.com>
2021-11-01 21:07:25 +0530

    post cache up

--- node-app/app.js
+++ node-app/app.js
@@ -3,6 +3,15 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
+app.use(function (req, res, next) {
+	if (!("JSONResponse" in res)) {
+		return next();
+	}
+
+	res.set("Cache-Control", "public, max-age=5000");
+	res.json(res.JSONResponse);
+});
+
 app.get("/", (req, res) => {
 	console.log("Hit on node");
 	res.send("Hello World!");
@@ -10,7 +19,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
 	console.log("Hit on node", req.body);
-	res.send("Hello World!");
+	res.send({ name: "test" });
 });
 
 app.listen(port, () => {
